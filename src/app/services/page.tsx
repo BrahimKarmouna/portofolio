a3bdb803c76ca7db2f21e86b4017eadf4138303a
Brahim Karmouna <157384891+BrahimKarmouna@users.noreply.github.com>
2024-08-21 14:02:10 +0100

    Update page.tsx

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -185,4 +185,32 @@ const Service: NextPage = () => {
   );
 };
 
-export default Service;
+const About: NextPage = () => {
+  return (
+    <main>
+      <section className="about section" id="about">
+        <div className="container">
+          <div className="row">
+            <div className="section-title padd-15">
+              <h2>About Me</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="about-content padd-15">
+              <div className="row">
+                <div className="about-text padd-15">
+                  <h2>
+                    I'm Karmouna Brahim a <span>passionate and dedicated developer</span>
+                  </h2>
+                  <p>
+                    Specializing in delivering high-quality software solutions as a freelancer. With over five completed projects, I've honed my skills in various programming languages and frameworks, positioning myself as an adept and versatile programmer ready to tackle any challenge.
+                  </p>
+                  <br />
+                  <p>
+                    <h2><span>Why Work With Me?</span></h2>
+                    Choosing to work with me means partnering with someone who is not only knowledgeable but also passionate about coding and development. I bring creativity, precision, and a commitment to delivering results that exceed expectations. My freelance experience has equipped me with the flexibility and resourcefulness to handle diverse projects, ensuring client satisfaction every step of the way.
+                    <br />
+                    Let's build something amazing together!
+                  </p>
+                </div>
+              </div
